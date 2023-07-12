413f399fc6cb0b50053e0e35a2e59820f7866eec
Rohit Sharma <rohitshar0801@gmail.com>
2023-07-12 11:21:40 +0530

    tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,12 @@ import { useState } from "react";
 import logo from "../assets/foodie.jpg";
 import { Link } from "react-router-dom";
 import useOnline from "../utils/useOnline";
+
 const Title = () => {
     return (
         <a href="/">
             <img
-                className="logo"
+                className="h-28 p-2"
                 alt="Logo"
                 src={logo} />
         </a>
@@ -19,23 +20,23 @@ const Header = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(true);
     const isOnline = useOnline();
     return (
-        <div className="">
+        <div className="flex justify-between bg-pink-500 shadow-lg ">
             <Title />
-            <div flex flex-row>
-                <ul>
-                    <li>
+            <div >
+                <ul className="flex py-2">
+                    <li className="px-2">
                         <Link to="/home">Home</Link>
                     </li>
-                    <li>
+                    <li className="px-2">
                         <Link to="/contact">Contact</Link>
                     </li>
-                    <li>
+                    <li className="px-2">
                         <Link to="/About">About</Link>
                     </li>
-                    <li>
+                    <li className="px-2">
                         <Link to="/cart">Cart</Link>
                     </li>
-                    <li>
+                    <li className="px-2">
                         <Link to="/instamart">Instamart</Link>
                     </li>
                 </ul>
@@ -51,7 +52,7 @@ const Header = () => {
                         </li>
 
                         <li>
-                            <h3>{isOnline ? "✅" : "🔴"}</h3>
+                            <h3 className="">{isOnline ? "✅" : "🔴"}</h3>
                         </li>
 
                     </ul>
