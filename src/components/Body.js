9fef7398beef8ef674a60017e8a3ae8d1f06a65b
Rohit Sharma <rohitshar0801@gmail.com>
2024-01-02 21:06:36 +0530

    cors-error

--- src/components/Body.js
+++ src/components/Body.js
@@ -20,7 +20,7 @@ const Body = () => {
   const getRestaurants = async () => {
     try {
       const data = await fetch(
-        "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139391&lng=77.2090212&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
 
 
       const json = await data.json();
