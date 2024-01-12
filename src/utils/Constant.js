1f5a48104b5655a6eaf149ec7fe0a46d2890d55b
Rohit Sharma <96370161+RohitSharma50@users.noreply.github.com>
2024-01-12 15:19:43 +0530

    Update Constant.js

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -3,11 +3,11 @@ export const IMG_CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lo
 
 
 
-export const MENU_API ="https://foodfire.onrender.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
+export const MENU_API ="https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 //"https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.29844139999999&lng=77.99313599999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const swiggy_menu_api_URL =
-  "https://foodfire.onrender.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
 
 
 export const options = {
